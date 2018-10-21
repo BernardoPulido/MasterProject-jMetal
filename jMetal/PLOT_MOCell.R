e3272e1347fe241c3f6edf10c6baaf8ebe28209e
Bernardo Pulido <lpulido@cicese.edu.mx>
2018-10-20 23:35:09 -0700

    Benchmarks VRP

--- jMetal/PLOT_MOCell.R
+++ jMetal/PLOT_MOCell.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/luis_/Documents/GitHub/MasterProject-jMetal/jMetal")
+setwd("C:/Users/luis_/Documents/GitHub/MasterProject-jMetal/jMetal/results")
 a <- read.table("FUN_MOCell.tsv")
 plot(a,lwd=2,xlab="Distancia", ylab="Penalización ambiental", las=1, panel.first=grid())
 abline(v=33, col=c("red"), lty=c(3), lwd=c(2))
