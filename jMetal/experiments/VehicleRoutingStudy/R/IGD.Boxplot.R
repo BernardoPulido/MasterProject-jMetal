fede38f1100a23327b1c126037c853a54eb77154
Bernardo Pulido <lpulido@cicese.edu.mx>
2018-10-21 21:09:52 -0700

    Experimentos Resultados Preliminares

---
+++ jMetal/experiments/VehicleRoutingStudy/R/IGD.Boxplot.R
@@ -0,0 +1,22 @@
+postscript("IGD.Boxplot.eps", horizontal=FALSE, onefile=FALSE, height=8, width=12, pointsize=10)
+resultDirectory<-"../data"
+qIndicator <- function(indicator, problem)
+{
+fileMOCell<-paste(resultDirectory, "MOCell", sep="/")
+fileMOCell<-paste(fileMOCell, problem, sep="/")
+fileMOCell<-paste(fileMOCell, indicator, sep="/")
+MOCell<-scan(fileMOCell)
+
+fileNSGAII<-paste(resultDirectory, "NSGAII", sep="/")
+fileNSGAII<-paste(fileNSGAII, problem, sep="/")
+fileNSGAII<-paste(fileNSGAII, indicator, sep="/")
+NSGAII<-scan(fileNSGAII)
+
+algs<-c("MOCell","NSGAII")
+boxplot(MOCell,NSGAII,names=algs, notch = FALSE)
+titulo <-paste(indicator, problem, sep=":")
+title(main=titulo)
+}
+par(mfrow=c(3,3))
+indicator<-"IGD"
+qIndicator(indicator, "VRP")
