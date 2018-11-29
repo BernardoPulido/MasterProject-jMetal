183cab9eeef12ef893965fe06f3a223614b912e1
Bernardo Pulido <lpulido@cicese.edu.mx>
2018-11-29 11:50:46 -0800

    Commit after first deadline

--- jMetal/PLOT_MOCell.R
+++ jMetal/PLOT_MOCell.R
@@ -4,3 +4,7 @@ plot(a,lwd=2,xlab="Distancia", ylab="Penalización ambiental", las=1, panel.first
 abline(v=33, col=c("red"), lty=c(3), lwd=c(2))
 abline(h=7, col=c("red"), lty=c(3), lwd=c(2))
 
+legend(42.5,12.5, legend = c("MOCell", "Soluciones óptimas"), 
+       col = c("black", "red"),
+       lty = c(0, 3), lwd = c(2, 1),
+       pch = c(1, NA), cex=0.9)
--- jMetal/PLOT_PF_.R
+++ jMetal/PLOT_PF_.R
@@ -1,7 +1,8 @@
 #setwd("C:/Users/luis_/Documents/GitHub/MasterProject-jMetal/jMetal/experiments/VehicleRoutingStudy/referenceFronts")
 setwd("C:/Users/luis_/Documents/GitHub/MasterProject-jMetal/jMetal/results")
 a <- read.table("VRP_.pf")
-plot(a,lwd=1, xlab="Distancia", pch=17, col="red", ylab="Penalización ambiental", las=1, panel.first=grid())
+plot(a,lwd=1, xlab="Distancia", pch=17, col="red", ylab="Penalización ambiental", las=1, panel.first=grid(), 
+     main="Resultados para grafo conexo de dimensión 100")
 
 setwd("C:/Users/luis_/Documents/GitHub/MasterProject-jMetal/jMetal/results")
 b <- read.table("FUN_MOCell_.tsv")
@@ -11,7 +12,7 @@ points(b, col="blue", lwd=2, pch=1, add =TRUE)
 c <- read.table("FUN_NSGAII_.tsv")
 points(c, col="green", pch=1, add =TRUE)
 
-legend(140, 90, legend=c("Frente aproximado de Pareto", "MOCell", "NSGAII"),
+legend(140, 90, legend=c("Frente artificial de Pareto", "MOCell", "NSGAII"),
        col=c("red", "blue", "green"), pch = c(17,1,1), cex=0.9)
 
 abline(v=94, col=c("red"), lty=c(3), lwd=c(2))
