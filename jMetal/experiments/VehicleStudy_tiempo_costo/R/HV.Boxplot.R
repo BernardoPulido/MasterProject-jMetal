6ea7c4186706b28fd319230716823a947ff7b81c
Luis Bernardo Pulido <lpulido@cicese.edu.mx>
2019-03-18 19:50:04 -0700

    Paper ISUM experiments

---
+++ jMetal/experiments/VehicleStudy_tiempo_costo/R/HV.Boxplot.R
@@ -0,0 +1,32 @@
+postscript("HV.Boxplot.eps", horizontal=FALSE, onefile=FALSE, height=8, width=12, pointsize=10)
+resultDirectory<-"../data"
+qIndicator <- function(indicator, problem)
+{
+fileMOCell<-paste(resultDirectory, "MOCell", sep="/")
+fileMOCell<-paste(fileMOCell, problem, sep="/")
+fileMOCell<-paste(fileMOCell, indicator, sep="/")
+MOCell<-scan(fileMOCell)
+
+fileNSGAII<-paste(resultDirectory, "NSGAII", sep="/")
+fileNSGAII<-paste(fileNSGAII, problem, sep="/")
+fileNSGAII<-paste(fileNSGAII, indicator, sep="/")
+NSGAII<-scan(fileNSGAII)
+
+fileSPEA2<-paste(resultDirectory, "SPEA2", sep="/")
+fileSPEA2<-paste(fileSPEA2, problem, sep="/")
+fileSPEA2<-paste(fileSPEA2, indicator, sep="/")
+SPEA2<-scan(fileSPEA2)
+
+fileWASFGA<-paste(resultDirectory, "WASFGA", sep="/")
+fileWASFGA<-paste(fileWASFGA, problem, sep="/")
+fileWASFGA<-paste(fileWASFGA, indicator, sep="/")
+WASFGA<-scan(fileWASFGA)
+
+algs<-c("MOCell","NSGAII","SPEA2","WASFGA")
+boxplot(MOCell,NSGAII,SPEA2,WASFGA,names=algs, notch = FALSE)
+titulo <-paste(indicator, problem, sep=":")
+title(main=titulo)
+}
+par(mfrow=c(3,3))
+indicator<-"HV"
+qIndicator(indicator, "VRP")
